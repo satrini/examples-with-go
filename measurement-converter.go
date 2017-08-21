fd77af4ccabd7f54a053a297f903df7e065ef6d9
Eduardo Henrique <eduardo_satriani@hotmail.com>
2017-08-21 00:25:53 -0300

    measurement-converter

---
+++ measurement-converter.go
@@ -0,0 +1,14 @@
+package main
+
+import "fmt"
+
+func main() {
+	var measures float64
+	fmt.Print("Enter a distance in meters: ")
+	fmt.Scanf("%f", &measures)
+	fmt.Printf("%f km\n", measures/1000)
+	fmt.Printf("%f hm\n", measures/100)
+	fmt.Printf("%.0f cm\n", measures*100)
+	fmt.Printf("%.0f mm\n", measures*1000)
+
+}
