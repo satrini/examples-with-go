313acc3af0f523d1b8246e98309d2eb9e1684048
Eduardo Henrique <eduardo_satriani@hotmail.com>
2017-08-21 02:47:01 -0300

    new algorithm

---
+++ calculating-discounts.go
@@ -0,0 +1,15 @@
+package main
+
+import "fmt"
+
+// Make an algorithm that reads the price of a product
+// and show its new price, with a 5% discount.
+
+func main() {
+	var price float64
+	fmt.Print("Price of the product: $")
+	fmt.Scanf("%f\n", &price)
+
+	discount := price - (price * 5 / 100)
+	fmt.Printf("The product with discount is: $%.2f", discount)
+}
